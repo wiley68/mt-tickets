9f6c55c3701473611728b4ac2acaa8c0fa22a866
wiley68 <ilko.iv@gmail.com>
2026-01-03 09:51:20 +0200

    init skeleton

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Bus Tickets Block theme functions.
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Засега не е нужно нищо тук.
+// По-късно може да добавим:
+// - register_block_pattern_category
+// - custom block styles
+// - wp_enqueue_block_style / custom assets
